c5c830a014813d050dff1ed37aaaaeb20c490ed0
unknown <aijazahmad9864@gmail.com>
2020-08-20 14:38:11 +0530

    add homepage to cache

--- sw.js
+++ sw.js
@@ -1,33 +1,42 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
+const cacheName = 'homepage';
+const resourcesToPrecache = [
+  '/',
+  'index.html'
+];
+
+self.addEventListener('install', event => {
+  console.log('service worker install event');
+  event.waitUntil(
+    caches.open(cacheName)
+    .then(cache => {
+      return cache.addAll(resourcesToPrecache);
+    })
+);
+})
+
+self.addEventListener('fetch', event =>{
+  event.respondWith(caches.match(event.request)
+  .then(cacheResponse => {
+    return cachedResponse || fetch(event.request);
+  })
+  );
+});
+
 
 workbox.routing.registerRoute(
-  /assets/,
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: 'static-files',
-    plugins: [
-      new workbox.expiration.ExpirationPlugin({
-        purgeOnQuotaError: true,
-        maxEntries: 100
-
-      })
-    ]
-  })
-)
-
-workbox.routing.registerRoute(
-  /index.html/,
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: 'homepage',
-    plugins: [
-      new workbox.expiration.ExpirationPlugin({
-        purgeOnQuotaError: true,
-        maxEntries: 100
-
-      })
-    ]
-  })
-)
-
+    /assets/,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'static-files',
+      plugins: [
+        new workbox.expiration.ExpirationPlugin({
+          purgeOnQuotaError: true,
+          maxEntries: 100
+  
+        })
+      ]
+    })
+  )
 
 
